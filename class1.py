ee0e1e19a2e0b679a35b178f8533296cb7675a95
AidaAsh <SoraMiadzaki@gmail.com>
2023-12-25 18:10:12 +0600

    lesson5

---
+++ class1.py
@@ -0,0 +1,5 @@
+class Hello:
+    title = 'Hello World'
+
+class Tprint(Hello):
+    pass
--- task.py
+++ task.py
@@ -1,3 +1,7 @@
 from art import tprint
 tprint("PYTHON")
 
+from class1 import Tprint
+
+ekz = Tprint()
+tprint(ekz.title)
